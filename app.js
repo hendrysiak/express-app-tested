fa168ec68e3d0433093e8579dd3ebfcff1130fd0
hendrysiak <lukasz.hendrysiak@gmail.com>
2019-08-19 17:21:04 +0200

    Add 404 handler

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.set('x-powered-by', false)
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
+});
+
+app.get('*', (req, res) => {
+    res.status(404);
+    res.send('not found')
 })
 
 exports.app = app;
--- app.test.js
+++ app.test.js
@@ -8,3 +8,12 @@ it('works', async() => {
     expect(response.status).toEqual(200);
     expect(response.text).toEqual('Hello world!');
 });
+
+it('handles pages thar are not found', async() => {
+    const response = await request(app).get('/whatever');
+
+    expect(response.status).toEqual(404);
+    expect(response.text).toEqual('not found');
+
+    // expect(response.text).toMatchSnapshot();
+})
